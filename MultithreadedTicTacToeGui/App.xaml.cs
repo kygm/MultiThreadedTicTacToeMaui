c20d22d90fbc99a793458bdcba92e2d1e19e450a
kygm <rickev20@gmail.com>
2025-01-26 18:23:29 -0600

    initial codebase upload

---
+++ MultithreadedTicTacToeGui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MultithreadedTicTacToeGui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
