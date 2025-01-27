92eff1836e7382c9ad3940c68cbeb0c54cb97124
kygm <rickev20@gmail.com>
2025-01-26 22:18:34 -0600

    Added random player and intitial position generation as well as a start game method and other related items

--- MultithreadedTicTacToeGui/Views/GameBoardPage.xaml.cs
+++ MultithreadedTicTacToeGui/Views/GameBoardPage.xaml.cs
@@ -11,4 +11,23 @@ public partial class GameBoardPage : ContentPage
 		_gameBoardVM = ServiceHelper.GetService<GameBoardVM>();
 		BindingContext = _gameBoardVM;
 	}
+
+	private bool _isLoaded = false;
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+		if(!_isLoaded)
+		{
+			await _gameBoardVM.StartGame();
+			_isLoaded = true;
+		}
+    }
+
+	private async void OnStartGamePressed(object sender, EventArgs e)
+	{
+		if(!_gameBoardVM.IsGameRunning)
+		{
+            await _gameBoardVM.StartGame();
+        }
+	}
 }
--- MultithreadedTicTacToeGui/Views/GameBoardVM.cs
+++ MultithreadedTicTacToeGui/Views/GameBoardVM.cs
@@ -21,6 +21,13 @@ namespace MultiThreadedTicTacToeGui.Views
             set => SetProperty(ref _gameBoardLabels, value);
         }
 
+        private bool _isGameRunning = false;
+        public bool IsGameRunning
+        {
+            get => _isGameRunning;
+            set => SetProperty(ref _isGameRunning, value);
+        }
+
         public GameBoardVM()
         {
             InitializeGameBoard();
@@ -32,16 +39,44 @@ namespace MultiThreadedTicTacToeGui.Views
             _populatedPlayerMatrix = new List<KeyValuePair<PlayerType, KeyValuePair<int, int>>>();
             _gameBoardLabels = new List<string>();
 
-            //Fill open board slots list
+            //Fill open board slots list and board labels list
             for(int rowIndex = 1; rowIndex <= 3; rowIndex++)
             {
                 for (int columnIndex = 1; columnIndex <= 3; columnIndex++)
                 {
                     _openBoardSlots.Add(new KeyValuePair<int, int>(rowIndex, columnIndex));
-                    GameBoardLabels.Add("X");
+                    GameBoardLabels.Add("");
                 }
             }
+        }
 
+        public async Task StartGame()
+        {
+            _isGameRunning = true;
+            //First select a randomNumberGenerator player - X or O
+            var randomNumberGenerator = new Random();
+            PlayerType startingPlayer = (PlayerType)randomNumberGenerator.Next(2); // Generates either 0 or 1
+
+            //Now select a first arbitrary randomNumberGenerator position
+            int initialPosition = randomNumberGenerator.Next(0, 9);
+
+            while(_isGameRunning)
+            {
+                await Application.Current.MainPage.DisplayAlert("Notice", $"Starting player {startingPlayer.ToString()}, starting position {initialPosition}", "Doge");
+                _isGameRunning = HasGameBeenCompletedCheckState();
+            }
+
+        }
+
+        /// <summary>
+        /// This method should return the state of the game - when the game completes due to a draw, a win 
+        /// by X, or a win by O, true should be returned
+        /// </summary>
+        /// <returns>State of the game - whether it's been completed or not</returns>
+        private bool HasGameBeenCompletedCheckState()
+        {
+            //Perform logic here to determine wheter a winning condition is present in the PopulatedPlayerMatrix collection
+            return false;
         }
     }
 
